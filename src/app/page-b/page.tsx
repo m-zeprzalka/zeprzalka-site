dbdae5636331379b1cef43a13aa2e06eb30074fc
m-zeprzalka <m@zeprzalka.pl>
2025-09-24 20:00:00 +0200

    wersja z podstawą bloga - wersja działająca - 3

--- src/app/page-b/page.tsx
+++ src/app/page-b/page.tsx
@@ -2,22 +2,10 @@ import { Hero } from "@/components/sections/Hero"
 import { Skills } from "@/components/sections/Skills"
 import { Gallery } from "@/components/sections/Gallery"
 import { Contact } from "@/components/sections/Contact"
-import { BlogE } from "@/components/alternative/BlogE"
-import { BlogF } from "@/components/alternative/BlogF"
-import { BlogG } from "@/components/alternative/BlogG"
-import { BlogH } from "@/components/alternative/BlogH"
-import { BlogI } from "@/components/alternative/BlogI"
-import { BlogJ } from "@/components/alternative/BlogJ"
 
 export default function Home() {
   return (
     <>
-      <BlogJ />
-      <BlogI />
-      <BlogH />
-      <BlogG />
-      <BlogF />
-      <BlogE />
       <Hero />
       <Skills />
       <Gallery />

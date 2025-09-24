5542fc314a15d915826dee4d17268d43c20bae2a
m-zeprzalka <m@zeprzalka.pl>
2025-09-24 19:58:31 +0200

    wersja z podstawą bloga - wersja działająca - 3

---
+++ src/app/blog/kategoria/page.tsx
@@ -0,0 +1,11 @@
+export default function CategoryPage() {
+  return (
+    <div className="prose mx-auto py-16 text-center">
+      <h1>Kategorie</h1>
+      <p>
+        Ta sekcja jest w przygotowaniu. Wkrótce pojawią się tutaj wpisy z
+        wybranej kategorii.
+      </p>
+    </div>
+  )
+}
